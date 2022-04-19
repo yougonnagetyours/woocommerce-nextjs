ded8062bae9db7b3a941f6144e7510bfd94fdb0d
Michał Potoczny <michal.potoczny.92@gmail.com>
2022-04-19 09:54:39 +0000

    Little page content changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,16 +31,7 @@ export default function Home(props: Props) {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        And this is a footer
       </footer>
     </div>
   );
@@ -61,6 +52,6 @@ export const getStaticProps: GetStaticProps = async () => {
     props: {
       products: wooCommerceProducts.data,
     },
-    // revalidate: 60 // regenerate page with new data fetch after 60 seconds
+    // revalidate: 60
   };
 };
