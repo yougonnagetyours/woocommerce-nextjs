f68e8323e48101d1a6acf53d8340044a8fbedb15
Michał Potoczny <michal.potoczny.92@gmail.com>
2022-04-20 09:31:02 +0000

    initial display of products

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['house-of-happiness.pl'],
+  },
 }
 
 module.exports = nextConfig
