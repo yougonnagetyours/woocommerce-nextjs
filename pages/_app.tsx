2fd4578f3a4bd821e360899af4224b5b342c1804
yougonnagetyours <michal.potoczny.92@gmail.com>
2022-04-23 11:46:17 +0200

    new styled components config: todo _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
-import GlobalStyle from '../styles/GlobalStyles'
-import { ThemeProvider } from 'styled-components'
+// import GlobalStyle from '../styles/GlobalStyles'
+// import { ThemeProvider } from 'styled-components'
 import { theme } from "../styles/theme"
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
+      {/* <GlobalStyle /> */}
+      {/* <ThemeProvider theme={theme}> */}
         <Component {...pageProps} />
-      </ThemeProvider>
+      {/* </ThemeProvider> */}
     </>
   );
 }
