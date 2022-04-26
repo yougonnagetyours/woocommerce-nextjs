6acd9dc9c9737cd3679059fc2ee0fe5d4a11504f
Michał Potoczny <michal.potoczny.92@gmail.com>
2022-04-26 10:35:11 +0000

    init config

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,14 +10,9 @@ import BasicLayout from "../layout/Basic";
 
 import ProductCard from "../components/productCard/productCard"
 
-// declare types for the functional component props //
 interface Props {
   products: Product[];
 }
-
-// const StyledH1 = styled.h1`
-//   font-family: ${(props) => props.theme.font.body};
-// `;
 
 const Title = styled.h1`
   color: red;
@@ -33,7 +28,6 @@ export default function Home(props: Props) {
   return (
     <BasicLayout>
       <Title>Hello World !</Title>
-      {/* <StyledH1>Welcome to Next.js!</StyledH1> */}
       { products.map((product) => {
         return <ProductCard product={product} key={product.id} />;
       })}
